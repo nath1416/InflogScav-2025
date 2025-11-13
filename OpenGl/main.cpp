686f164d3b20d2780deef5bbebb894c085dd521d
x1-carbon <bougienath@gmail.com>
2025-11-13 10:58:11 -0500

    s

--- OpenGl/main.cpp
+++ OpenGl/main.cpp
@@ -128,7 +128,7 @@ int main()
 
         // MVP matrices
         glm::mat4 model = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, -3.0f));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
 
         unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
